e7a4fa398d7b8486d0a86bbf3dad2d4a62af5fc7
carpet1994 <94118860+carpet1994@users.noreply.github.com>
2026-03-30 04:04:41 +0200

    Add files via upload

--- p4a_recipes/pygame/__init__.py
+++ p4a_recipes/pygame/__init__.py
@@ -1,25 +1,24 @@
 """
-Recipe pygame custom: forza pygame 2.6.1 per evitare il bug longintrepr.h
-presente in pygame <= 2.5.x con Python 3.11.10+.
+Recipe pygame custom: usa il sdist PyPI di pygame 2.6.1 invece del
+tarball GitHub. Il sdist contiene i file .c già generati da Cython,
+quindi la build NON richiede Cython nell'hostpython.
 
-Non importa la classe per nome (il nome varia tra versioni di p4a),
-ma la recupera dinamicamente dal modulo originale.
+Problema risolto: quando si scarica pygame da GitHub (archive/{version}.tar.gz)
+i file .pyx Cython sono presenti ma i .c pre-generati no, quindi
+setup.py chiama cython per rigenerarli. Il sdist PyPI invece include
+già i .c, e setup.py li usa direttamente (USE_CYTHON=0 implicito).
 """
 import importlib
-import sys
 
-# Carica il modulo originale della recipe pygame da p4a
 _orig = importlib.import_module("pythonforandroid.recipes.pygame")
-
-# Trova la classe Recipe definita nel modulo (convenzionalmente l'ultima
-# sottoclasse di Recipe definita nel file, assegnata a `recipe`)
 _base_recipe_cls = type(_orig.recipe)
 
 
-class PygameFixedRecipe(_base_recipe_cls):
+class PygameSdistRecipe(_base_recipe_cls):
     version = "2.6.1"
-    url = "https://github.com/pygame/pygame/archive/{version}.tar.gz"
-    md5sum = None  # disabilita checksum per compatibilità mirror
+    # sdist da PyPI: contiene i .c pre-generati, NO Cython richiesto
+    url = "https://files.pythonhosted.org/packages/source/p/pygame/pygame-{version}.tar.gz"
+    md5sum = None  # disabilita checksum
 
 
-recipe = PygameFixedRecipe()
+recipe = PygameSdistRecipe()
